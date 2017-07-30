8a7f19343baf29d851b62f2e2f58114615323e5f
Nihal George <nihaleg@gmail.com>
2017-07-30 18:07:05 +0000

    Add RESTful API

--- index.js
+++ index.js
@@ -12,79 +12,131 @@ admin.initializeApp({
 });
 
 var db = admin.database();
-var ref = db.ref('Gautam');
-
+var ref = db.ref('Gautam').child('Cuisine')
+var ACCESS_TOKEN;
 
 app.get('/', function(req, res) {
-  res.send('Hello World!')
-  console.log("Hello World")
+  res.send('hello world')
 })
 
 app.get('/initial', function(req, res) {
-  ref.child('initial').on("value", function(snapshot) {
+  res.send("Greek");
+  /*ref.child('initial').on("value", function(snapshot) {
     console.log(snapshot.val());
-    res.send(snapshot.val());
-  }, function (errorObject) {
+    //res.send(snapshot.val());
+  }, function(errorObject) {
     console.log("The read failed: " + errorObject.code);
-  });
+  });*/
 });
 
 app.get('/cuisine', function(req, res) {
-  console.log('test')
+  
+  var options = {
+    method: 'POST',
+    url: 'https://api.yelp.com/oauth2/token',
+    qs: {
+      grant_type: 'client_credentials',
+      client_id: 'vRSLRJhyDLQuyMD6K_4bBA',
+      client_secret: '6VEdUwbuuvzmicA62x06asYwxX60r0rSav5SeXPkxDbzLsGaHDdZWM1TSnKpp4f3'
+    }
+  };
+  request(options, function(error, response, body) {
+    console.log(body);
+    body = JSON.parse(body);
+    ACCESS_TOKEN = body.access_token;
+    console.log(ACCESS_TOKEN);
+    
+  });
+  console.log("Hello World");
+  
+  console.log('test');
+  var $data = {}
 
   var options = {
     method: 'GET',
     url: 'http://angelhacks17-imitra.c9users.io:8080/'
   }
-  
+
   var recommended;
   
   request(options, function(error, response, body) {
-      recommended = body;
+   recommended = body;
+   var options = {
+    method: 'GET',
+    url: 'https://api.yelp.com/v3/businesses/search',
+    qs: {
+      term: 'restaurants',
+      latitude: 37.563106,
+      longitude: -122.325028,
+      radius: 2500,
+      categories: recommended.toLowerCase(),
+      limit: 3,
+      sort_by: 'rating',
+      open_now: true
+    },
+    'auth': {
+      'bearer': ACCESS_TOKEN
+    }
+  };
+  
+  request(options, function(error, response, body) {
+      body = JSON.parse(body);
+      console.log(body.businesses[0]);
+      var business = body.businesses[0];
+      var recommendedString = '' + recommended;
+      
+      var data = {
+          [recommendedString]: {
+            'Meals': body.businesses
+          }
+      }
+      
+      ref.update(data);
+      res.send(data);
+    });
+    
+    
+  
   });
   
+   
 
+
+});
+
+app.get('/order', function(req, res) {
   var options = {
-    method: 'GET',
-    url: 'https://api.eatstreet.com/publicapi/v1/restaurant/search',
+    method: 'POST',
+    url: 'https://api.eatstreet.com/publicapi/v1/send-order',
     qs: {
-      method: 'delivery',
-      'pickup-radius': '5',
-      search: recommended,
-      'street-address': '500 Ben Franklin Ct., San Mateo, CA, 94401',
-      'access-token': 'eeef884f9fe78292'
-    },
-    headers: {
-      'postman-token': 'b2176e9b-640d-3d43-a302-ee245ccb13b0',
-      'cache-control': 'no-cache',
-      'content-type': 'multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW'
+      restarauntApiKey: '90fd4587554469b1f15b4f2e73e761809f4b4bcca52eedca',
+      'items': [{
+        "apiKey": "35194",
+        "customizationChoices": [{
+          "apiKey": "77227393"
+        }],
+        "comments": "Pile it high!"
+      }],
+      'method': "delivery",
+      'payment': 'card',
+      'access-token': 'eeef884f9fe78292',
+      "test": "false",
+      "card": {
+        "apiKey": null
+      },
+      "address": {
+        "apiKey": null
+      },
+      "recipient": {
+        "apiKey": "485ca34bedf9153e7ecdb0c1c698d2cee41ee9406039e889"
+      }
     },
   };
 
-  var restaurant = null
-
   request(options, function(error, response, body) {
     if (error) throw new Error(error);
-    body = JSON.parse(body).restaurants;
-    var name = body[0].name;
-    var apiKey = body[0].apiKey;
+    console.log(body);
 
-    var request = require("request");
-
-    var options = {
-      method: 'GET',
-      url: 'https://api.eatstreet.com/publicapi/v1/restaurant/'+apiKey+'/menu',
-      qs: {
-        'access-token': 'eeef884f9fe78292',
-        includeCustomizations: 'false'
-      },
-    };
-    request(options, function(error, response, body) {
-      if (error) throw new Error(error);
-      body = JSON.parse(body);
-      console.log('output' + JSON.stringify(body[0].items[0]));
-      res.send(JSON.stringify(body[0].items[0]));
-    });
   });
 });
 
